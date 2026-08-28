29c4b607dccf5e6b8dc28399824150c750af0a3e
yuridiatech <yuri.alec@diatechsistemas.com.br>
2026-08-28 14:45:24 -0300

    ajuste

--- public/js/app.js
+++ public/js/app.js
@@ -33586,7 +33586,7 @@ __webpack_require__.r(__webpack_exports__);
 
 var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0___default()(function(i){return i[1]});
 // Module
-___CSS_LOADER_EXPORT___.push([module.id, "\n.user-menu .dropdown-item[data-v-0851419a]:hover,\n.user-menu .dropdown-item[data-v-0851419a]:focus {\n    background-color: rgba(var(--bs-primary-rgb), 0.1);\n}\n", ""]);
+___CSS_LOADER_EXPORT___.push([module.id, "\n.user-menu .dropdown-item[data-v-0851419a]:hover,\r\n.user-menu .dropdown-item[data-v-0851419a]:focus {\r\n    background-color: rgba(var(--bs-primary-rgb), 0.1);\n}\r\n", ""]);
 // Exports
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
 
